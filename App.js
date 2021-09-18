e0f91f934c3d65721af72bd01a94cc47eb0b67a0
Shaurya_Kharb <228geetakharb@gmail.com>
2021-09-18 09:08:46 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs' 
+import EnterDetails from './screens/EnterDetails'
+import ReadDetails from './screens/ReadDetails'
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return(
+      <SafeAreaProvider>
+      <AppContainer />
+      </SafeAreaProvider>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const TabNavigator = createBottomTabNavigator({
+  EnterDetails: EnterDetails,
+  ReadDetails: ReadDetails
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "EnterDetails"){
+        return(
+          <Image
+          source={require("./assets/job1.png")}
+          style={{width:40, height:40}}
+        />
+        )
+        
+      }
+      else if(routeName === "ReadDetails"){
+        return(
+          <Image
+          source={require("./assets/job2.webp")}
+          style={{width:40, height:40}}
+        />)
+        
+      }
+    }
+  })
+}
+  )
+
+const AppContainer = createAppContainer(TabNavigator);
+
+
+
